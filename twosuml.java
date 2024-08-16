fffc7d79f8b97a5658f3bbc448ddafe0fe2500fa
RaajeshKumaar17 <raajeshvelusamy@gmail.com>
2024-08-16 20:12:14 +0530

    added

--- twosuml.java
+++ twosuml.java
@@ -1,3 +1,21 @@
 public class twosuml {
+    public int[] twoSum(int[] numbers, int target) {
+        int[] result=new int[2];
+        int num1=0;
+        int sum=0;
+        for(int i=0;i<numbers.length;i++){
+         num1=numbers[i];
+         for(int j=i+1;j<numbers.length;j++){
+            sum=num1+numbers[j];
+            if(sum==target){
+                result[0]=i;
+                result[1]=j;
+            }
+         }
+        }
+       return result;
+    }
+}       
+           
     
-}
+
